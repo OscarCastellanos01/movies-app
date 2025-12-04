4779b56af629915a2547be5dc82395f7ba370edc
Oscar Castellanos <oscareliascastellanoscordero@gmail.com>
2025-12-03 20:03:43 -0600

    Mapear respuestas a objetos personalizados

--- core/actions/movies/now-playing.action.ts
+++ core/actions/movies/now-playing.action.ts
@@ -1,10 +1,14 @@
 import { MovieDBMvoiesResponse } from '@/infrastructure/interfaces/moviedb-response';
+import { MovieMapper } from '@/infrastructure/mappers/movie.mappers';
 import { movieApi } from '../../api/movie-api';
 export const nowPlayingAction = async() => {
     try {
         const { data } = await movieApi.get<MovieDBMvoiesResponse>("/now_playing");
-            console.log(JSON.stringify(data,null,2));
-        return []
+        // console.log(JSON.stringify(data,null,2));
+        // const movies = data.results.map((movie) => MovieMapper.fromTheMovieDBToMovie(movie));
+        const movies = data.results.map(MovieMapper.fromTheMovieDBToMovie);
+        console.log(movies);
+        return movies
     } catch (error) {
         console.log(error);
         throw 'Cannot load now playing movies.'
