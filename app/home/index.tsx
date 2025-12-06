c0368888aec642b63d51e6e1f874c07394ca4820
Oscar Castellanos <oscareliascastellanoscordero@gmail.com>
2025-12-05 20:39:52 -0600

    Integracion Carousel

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,15 +1,28 @@
+import { ActivityIndicator, Text, View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+import MainSlideshow from '@/presentation/components/MainSlideshow';
 import { useMovies } from '@/presentation/hooks/useMovies';
-import { Text, View } from 'react-native';
 
 const HomeScreen = () => {
+    const safeArea = useSafeAreaInsets();
     const { nowPlayingQuery } = useMovies();
 
-  return (
-    <View>
-      <Text>HomeScreen</Text>
-      <Text>{JSON.stringify(nowPlayingQuery.data)}</Text>
-    </View>
-  )
+    if (nowPlayingQuery.isLoading) {
+      return (
+        <View className='justify-center items-center flex-1'>
+          <ActivityIndicator color="purple" size={40}/>
+        </View>
+      )
+    }
+
+    return (
+      <View className='mt-5' style={{ paddingTop: safeArea.top }}>
+        <Text className='text-3xl font-bold px-4 mb-2'>HomeScreen</Text>
+        {/* Carousel de imagenes */}
+        <MainSlideshow movies={ nowPlayingQuery.data ?? [] } />
+      </View>
+    )
 }
 
 export default HomeScreen
