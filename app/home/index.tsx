c98c8b3b35bb2ff28b2fad461e0c24fc17de95f9
Oscar Castellanos <oscareliascastellanoscordero@gmail.com>
2025-12-05 21:36:07 -0600

    FlatList Horizontal

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -2,11 +2,12 @@ import { ActivityIndicator, Text, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import MainSlideshow from '@/presentation/components/movies/MainSlideshow';
+import MovieHorizontalList from '@/presentation/components/movies/MovieHorizontalList';
 import { useMovies } from '@/presentation/hooks/useMovies';
 
 const HomeScreen = () => {
     const safeArea = useSafeAreaInsets();
-    const { nowPlayingQuery } = useMovies();
+    const { nowPlayingQuery, popularQuery } = useMovies();
 
     if (nowPlayingQuery.isLoading) {
       return (
@@ -17,12 +18,14 @@ const HomeScreen = () => {
     }
 
     return (
-      <View className='mt-5' style={{ paddingTop: safeArea.top }}>
-        <Text className='text-3xl font-bold px-4 mb-2'>MoviesApp</Text>
+      <View className="mt-5" style={{ paddingTop: safeArea.top }}>
+        <Text className="text-3xl font-bold px-4 mb-2">MoviesApp</Text>
         {/* Carousel de imagenes */}
-        <MainSlideshow movies={ nowPlayingQuery.data ?? [] } />
+        <MainSlideshow movies={nowPlayingQuery.data ?? []} />
+        {/* Popular */}
+        <MovieHorizontalList title='Populares' movies={popularQuery.data ?? []} />
       </View>
-    )
+    );
 }
 
 export default HomeScreen
---
+++ core/actions/movies/popular.action.ts
@@ -0,0 +1,17 @@
+import { MovieDBMvoiesResponse } from '@/infrastructure/interfaces/moviedb-response';
+import { MovieMapper } from '@/infrastructure/mappers/movie.mappers';
+import { movieApi } from '../../api/movie-api';
+
+export const popularMoviesAction = async() => {
+    try {
+        const { data } = await movieApi.get<MovieDBMvoiesResponse>("/popular");
+        // console.log(JSON.stringify(data,null,2));
+        // const movies = data.results.map((movie) => MovieMapper.fromTheMovieDBToMovie(movie));
+        const movies = data.results.map(MovieMapper.fromTheMovieDBToMovie);
+        // console.log(movies);
+        return movies
+    } catch (error) {
+        console.log(error);
+        throw 'Cannot load now playing movies.'
+    }
+}
